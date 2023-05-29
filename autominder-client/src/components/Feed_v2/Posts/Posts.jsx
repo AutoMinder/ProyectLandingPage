1a3e14874fa8d6b5b3fdd9c42666daf9f026695e
AleGC019 <00083521@uca.edu.sv>
2023-05-29 15:52:23 -0600

    :memo: Patch

--- autominder-client/src/components/Feed_v2/Posts/Posts.jsx
+++ autominder-client/src/components/Feed_v2/Posts/Posts.jsx
@@ -1,20 +1,64 @@
 import SinglePost from '../SinglePost/SinglePost';
+
+
+import { useConfigContext } from "../../../contexts/ConfigContext";
+import { useState, useEffect } from "react";
+import { useUserConext } from "../../../contexts/UserContext";
+import { toast } from "react-toastify";
+import axios from "axios";
+
+
 import classes from './Posts.module.scss';
+
 
 const Post = ({posts = []}) => {
 
-    const ereasePost = (id) => {
-        
+    const {startLoading, stopLoading} = useConfigContext();
+    const {token, user} = useUserConext();
+
+ 
+
+    const deletePost = async (id) => {
+        try{
+
+            startLoading();
+            console.log(id)
+            await axios.patch(`/post/visibility/${id}`, {}, { headers: {authorization: `Bearer ${token}` }});       
+            toast.success('Post deleted');
+
+        }
+        catch(error){
+            const {status} = error.response || {status: 500};
+
+            const msg = {
+                400: 'Bad request',
+                404: 'Post not found',
+                500: 'Internal server error'
+            }
+
+            toast.error(msg[status.toString()] || 'Error deleting post');
+        }
+        finally{
+            stopLoading();
+        }
+
     }
 
+    const onDeletePostHandler = async (id) => {
+        await deletePost(id.id);
+    }
+
+
+    
+    
     const mappedPosts = posts.map(post => {
         return(
             <SinglePost
                 key={post._id}
+                id={post._id}
                 car_name= {post.car_name}
                 user = {post.user.username} 
-                rol = {post.user.roles}
-                
+                onDeletePost={onDeletePostHandler}
             />
         );
     });
--- autominder-client/src/views/FeedView/FeedView.jsx
+++ autominder-client/src/views/FeedView/FeedView.jsx
@@ -1,7 +1,7 @@
 import NewPostForm from "../../components/Feed_v2/NewPostForm/NewPostForm";
 import Post from "../../components/Feed_v2/Posts/Posts";
 import { useConfigContext } from "../../contexts/ConfigContext";
-
+import NotFoundView_v2 from '../../views/NotFoundView_v2/NotFoundView_v2';
 
 import classes from './FeedView.module.scss';
 
@@ -45,8 +45,6 @@ const FeedView = () => {
     const savePost = async (title, description, image) => {
         try{
             
-            
-
             startLoading();
 
             await axios.post('/post', { title, description, image }, { headers: {authorization: `Bearer ${token}` }});
@@ -68,6 +66,37 @@ const FeedView = () => {
         }
     }
 
+    const deletePost = async (id) => {
+        try{
+
+            startLoading();
+
+            await axios.patch(`/post/visibility/${id}`, { headers: {authorization: `Bearer ${token}` }});
+            toast.success('Post deleted');
+
+        }
+        catch(error){
+            const {status} = error.response || {status: 500};
+
+            const msg = {
+                400: 'Bad request',
+                404: 'Post not found',
+                500: 'Internal server error'
+            }
+
+            toast.error(msg[status.toString()] || 'Error deleting post');
+        }
+        finally{
+            stopLoading();
+        }
+
+    }
+
+    const onDeletePostHandler = async (id) => {
+        await deletePost(id);
+        fetchPosts();
+    }
+
     const onAddPostHandler = async (title, description, image) => {
         
 
@@ -78,13 +107,21 @@ const FeedView = () => {
 
     return(
         <div className={classes["news-publish"]}>
-            <div className={classes["feed-wrapper"]}>
-        
-                    {/* Lista de posts - Main Feed - Muestra de elementos */}
-                    <Post posts={posts}/>
-            </div>
-
-           
+            {
+                user.roles == 'user'?
+                <>  
+                    {/* Post de los carros con el boton eliminar */}
+                    <div className={classes["feed-wrapper"]}>
+                        <Post posts={posts} />
+                    </div>
+                
+                </>
+                :
+                <>
+                    {<NotFoundView_v2/>}
+                    
+                </>
+            }           
         </div>
     )
 }
